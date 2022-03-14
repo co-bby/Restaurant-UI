91561f2fc53988c0d93e11da5edf8f23b258cfe7
co-bby <nkumsagoefredrick@gmail.com>
2022-03-14 08:35:20 -0400

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Navbar.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+import images from '../../constants/images'
+import './Navbar.css'
+
+const Navbar = () => <div>Navbar</div>
+
+export default Navbar
