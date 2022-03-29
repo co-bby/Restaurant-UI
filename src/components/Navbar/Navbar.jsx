92e9a4a686ed7705b7b6d4ed4f034fa4d9c2362e
co-bby <nkumsagoefredrick@gmail.com>
2022-03-29 14:56:43 -0400

    about-us compoent on small screen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,6 +14,23 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         <img src={images.gericht} alt="app__logo" />
       </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#about">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#awards">Awards</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
       <div className="app__navbar-login">
         <a href="#login" className="p__opensans">
