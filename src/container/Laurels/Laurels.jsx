00d9d9d0b3a8d4cc6a0c122d2faf1e51abb9ccea
co-bby <nkumsagoefredrick@gmail.com>
2022-04-27 12:10:51 -0400

    added findus component

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SubHeading, MenuItem } from '../../components'
+import { SubHeading } from '../../components'
 import { images, data } from '../../constants'
 
 import './Laurels.css'
